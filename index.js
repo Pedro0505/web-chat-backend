a0cf8b6091a7bd273fd34e2d299d5af2c846907f
Pedro Henrique <pedrohrds05@hotmail.com>
2022-03-07 10:36:05 -0300

    fix: tentando o deploy

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'https://web-chat-front-swart.vercel.app/',
+    origin: 'https://web-chat-front-swart.vercel.app/*',
     methods: ['GET', 'POST'],
   },
 });
