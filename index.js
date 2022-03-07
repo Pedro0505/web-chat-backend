ff5a62dc02a7aac449eeb4c2f5550211244eb9fc
Pedro Henrique <pedrohrds05@hotmail.com>
2022-03-07 09:48:21 -0300

    feat: adicionando parte do server em um novo repositorio

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado: ${socket.id}`);
+
+  socket.on('join_room', (data) => {
+    socket.join(data);
+    console.log(`Usuário Id: ${socket.id} Sala: ${data}`);
+  });
+
+  socket.on('send_message', (data) => {
+    socket.to(data.room).emit('receive_message', data);
+    console.log(`O usuário ${data.user} Sala: ${data.room} Enviou a message: ${data.currMessage}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Usuário desconectado', socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log('Online');
+});
