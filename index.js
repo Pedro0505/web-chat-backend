315f7f94b77cc7c592e457f3a38dc99146617631
Pedro Henrique <pedrohrds05@hotmail.com>
2022-03-07 10:29:13 -0300

    feat: adicionando url do deploy

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: 'https://web-chat-front-swart.vercel.app/',
     methods: ['GET', 'POST'],
   },
 });
